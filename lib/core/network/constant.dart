8363eed8e460d865b633b16bec7282134dec8f0a
mohamedSaber <mohamed_sabry@grand-community.com>
2023-03-01 09:33:15 +0200

    payment finish

--- lib/core/network/constant.dart
+++ lib/core/network/constant.dart
@@ -1,20 +1,20 @@
 class ApiContest {
   static const String baseUrl = 'https://accept.paymob.com/api';
-  static const String paymentApiKey = "ENTER_YOUR PAYMENT_API_KEY";
+  static const String paymentApiKey = "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TWpVME56WTJMQ0p1WVcxbElqb2lhVzVwZEdsaGJDSjkuT2VxUmZjOWotYUxjZF9IT3VmWjhZTlNueXc3UXZDRVVybzMwbzh1b3VsaXJ1eWp0cjBpakhPTDV4ME1BdFczRU8xZlZCWHVBSUEwZG5FeDVzbHJ5b1E=";
   static const String getAuthToken = '/auth/tokens';
   static const getOrderId = '/ecommerce/orders';
   static const getPaymentRequest = '/acceptance/payment_keys';
   static const getRefCode = '/acceptance/payments/pay';
   static String visaUrl =
-      '$baseUrl/acceptance/iframes/424000?payment_token=$finalToken';
+      '$baseUrl/acceptance/iframes/438453?payment_token=$finalToken';
   static String paymentFirstToken = '';
 
   static String paymentOrderId = '';
 
   static String finalToken = '';
 
-  static const String integrationIdCard = 'ENTER_YOUR_INTEGRATION_ID';
-  static const String integrationIdKiosk = 'ENTER_YOUR_INTEGRATION_ID';
+  static const String integrationIdCard = '2479543';
+  static const String integrationIdKiosk = '3468304';
 
   static String refCode = '';
 }
